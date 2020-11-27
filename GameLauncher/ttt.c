edcae280923217e799fd3c375104f51a19dc5df7
Benno Roth <benno.roth@siemens.com>
2020-11-27 08:34:08 +0000

    Update ttt.c

--- GameLauncher/ttt.c
+++ GameLauncher/ttt.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 //#include<Windows.h>
-#define RED     "\x1b[31m"
+
 #define GREEN   "\x1b[32m"
 #define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
 #define RESETCOLOR   "\x1b[0m"
 char field[3][3], nickname1[20], nickname2[20], puffer;
